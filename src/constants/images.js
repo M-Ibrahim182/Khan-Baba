2f2155d13689ea77b1df10282336e533ae517370
M.Ibrahim <m.ibrahimyousaf182@gmail.com>
2024-10-06 10:19:53 +0500

    optization

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,25 +1,24 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import bowl from '../assets/bowl.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
+import bg from '../assets/bg.webp';
+import chef from '../assets/chef.webp';
+import bowl from '../assets/bowl.webp';
+import gallery01 from '../assets/gallery01.webp';
+import gallery02 from '../assets/gallery02.webp';
+import gallery03 from '../assets/gallery03.webp';
+import gallery04 from '../assets/gallery04.webp';
+import knife from '../assets/knife.webp';
+import logo from '../assets/logo.webp';
+import menu from '../assets/menu.webp';
+import overlaybg from '../assets/overlaybg.webp';
 import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
+import welcome from '../assets/welcome.webp';
+import findus from '../assets/findus.webp';
 import laurels from '../assets/laurels.webp';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import main_logo from '../assets/main_logo.png';
+import award01 from '../assets/award01.webp';
+import award02 from '../assets/award02.webp';
+import award03 from '../assets/award03.webp';
+import award05 from '../assets/award05.webp';
+import sign from '../assets/sign.webp';
+import main_logo from '../assets/main_logo.webp';
 
 const images=  {
   bg,
@@ -42,7 +41,6 @@ const images=  {
   award03,
   award05,
   sign,
-  quote,
   main_logo,
 };
 export default images;
